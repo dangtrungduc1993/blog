c6de41bb75497dc7f12d8a2eb8d0064553c9306b
dangtrungduc1993 <trungduc.hou@gmail.com>
2022-06-17 00:52:41 +0700

    finish login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,3 +1,8 @@
-class Admin::SessionController < Devise::SessionController
+class Admin::SessionsController < Devise::SessionsController
   layout "admin/unauthenticate"
+
+  def destroy
+    flash[:success] = I18n.t("devise.sessions.signed_out") if sign_out current_admin_admin
+    redirect_to new_admin_admin_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  # root to: "home#index"
+
+  namespace :admin do
+    root "home#index"
+    devise_for :admins, path: "", path_names: { sign_in: "login", sign_out: "logout" },
+      skip: [:registrations, :passwords]
+  end
 end
